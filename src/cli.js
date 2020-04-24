a67ab258a9ca027856b18ae501e249fefbede766
Roman Chechel <chechel-roman@lenta.ru>
2020-04-25 06:37:24 +0700

    fixed  for linter

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const getUserName = () => readlineSync.question('May I have your name? ');
-const getUserAnswer = () => readlineSync.question('Your answer ');
+// const getUserAnswer = () => readlineSync.question('Your answer ');
 const greetUser = () => {
   console.log('Welcome to the Brain Games!');
   const userName = getUserName();
