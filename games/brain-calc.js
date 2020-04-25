b53bbbae4370ab14a2c78d919ffed7765d539f4f
Roman Chechel <chechel-roman@lenta.ru>
2020-04-25 07:33:12 +0700

    refactored brain-calc game

--- games/brain-calc.js
+++ games/brain-calc.js
@@ -11,6 +11,30 @@ import {
 const sum = (num1, num2) => num1 + num2;
 const mult = (num1, num2) => num1 * num2;
 const sub = (num1, num2) => num1 - num2;
+const getRandomOperation = (operSum, operSub, operMult) => {
+  const number1 = getRandomNumber(1, 20);
+  const number2 = getRandomNumber(1, 20);
+  const operations = [operSum, operSub, operMult];
+  const random = getRandomNumber(0, operations.length - 1);
+  const randomOperation = operations[random];
+  const idxOfRandomOperation = operations.indexOf(randomOperation);
+
+  switch (idxOfRandomOperation) {
+    case 0:
+      console.log(`Question: ${number1} + ${number2}`);
+      break;
+    case 1:
+      console.log(`Question: ${number1} - ${number2}`);
+      break;
+    case 2:
+      console.log(`Question: ${number1} * ${number2}`);
+      break;
+    default:
+      break;
+  }
+
+  return randomOperation(number1, number2).toString();
+};
 
 const playBrainCalcGame = () => {
   const userName = greetUser();
@@ -18,29 +42,8 @@ const playBrainCalcGame = () => {
   let correctAnswers = 0;
 
   while (correctAnswers < maxNumberofRounds) {
-    const number1 = getRandomNumber(1, 20);
-    const number2 = getRandomNumber(1, 20);
-    const operations = [sum, sub, mult];
-    const random = getRandomNumber(0, operations.length - 1);
-    const randomOperation = operations[random];
-    const idxOfRandomOperation = operations.indexOf(randomOperation);
-
-    switch (idxOfRandomOperation) {
-      case 0:
-        console.log(`Question: ${number1} + ${number2}`);
-        break;
-      case 1:
-        console.log(`Question: ${number1} - ${number2}`);
-        break;
-      case 2:
-        console.log(`Question: ${number1} * ${number2}`);
-        break;
-      default:
-        break;
-    }
-
+    const correctAnswer = getRandomOperation(sum, sub, mult);
     const answer = (getUserAnswer());
-    const correctAnswer = randomOperation(number1, number2).toString();
 
     if (answer !== correctAnswer) {
       printWhenIncorrectAnswer(answer, correctAnswer, userName);
